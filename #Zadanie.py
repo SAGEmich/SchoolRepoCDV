96e68354a827d00285ce962aa9e2118c5494978b
Michał Znajkiewicz <mznajkiewicz@edu.cdv.pl>
2020-05-19 10:03:54 +0200

    MoreEx

---
+++ #Zadanie.py
@@ -0,0 +1,13 @@
+#Zadanie
+
+x=input("Podaj x: ")
+x=input("Podaj x: ")
+x=input("Podaj x: ")
+
+
+if x >= y and x>=z:
+    print(f'Wartość: {x} jest największa')
+    elif y >= x and y >= z:
+print(f'Wartość: {y} jest największa')
+        else:
+            print(f'Wartość: {z} jest największa')
